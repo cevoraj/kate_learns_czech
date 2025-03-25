ac227ba3de1a54b8e61182e70b7af84c5a9a54af
cevoraj <jiri.cevora@gmail.com>
2025-03-25 14:12:55 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+
+from streamlit_gsheets import GSheetsConnection
+
+# Establish the connection
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Read the data
+df = conn.read()
+
+# Display the data
+for row in df.itertuples():
+    st.write(f"{row}")
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
