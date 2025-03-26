409d2faf4be07aadf66578f1357d389ef4e23bc8
cevoraj <jiri.cevora@gmail.com>
2025-03-26 13:57:01 +0000

    v1 release

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 from openai import OpenAI
+import json
+
+
+fullExample = ""
 
 # Establish the connection
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -26,7 +30,7 @@ client = OpenAI(api_key=api_key)
 
 # Set a default model
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4o"
 
 tab1, tab2 = st.tabs(["Česky-Anglicky", "Anglicky-Česky"])
 
@@ -35,38 +39,57 @@ def sample():
     return df.sample(weights=df["probability"])
 
 def getExample(word):
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": "user", "content": f"Napiš mi jednoduchou českou větu která užívá slovo {word}"}
-            ],
-            stream=True,
-        )
+    global fullExample
+
+    message = client.chat.completions.create(
+        model=st.session_state["openai_model"],
+        messages=[
+            {"role": "user", "content": f"Napiš mi jednoduchou českou větu která užívá slovo {word}. Věta by měla mít maximálně 8 slov a  měla by jen používat jednoduché gramatické koncepty vhodné pro studenta českého jazyku na úrovní B1. Slovo {word} by mělo být podtrženo markdownem."},
+        ],
+    )
+    return message.choices[0].message.content
+
+def getTranslation(text):
+    message = client.chat.completions.create(
+        model=st.session_state["openai_model"],
+        messages=[
+            {"role": "user", "content": f"Přelož mi tuto větu do angličtiny: {text}"},
+        ],
+    )
         
-    return stream
+    return message.choices[0].message.content
             
 
 with tab1:
     if "sampleWord" not in st.session_state:
         st.session_state["sampleWord"] = sample()
-        st.write(st.session_state["sampleWord"]["Czech"])
-
+    if "state" not in st.session_state:
+        st.session_state["state"] = "init"
+    
     
 
     if st.button("Nové slovíčko"):
         st.session_state["sampleWord"] = sample()
-        st.write(st.session_state["sampleWord"]["Czech"])
+        st.session_state["state"] = "new"
+
+    st.write(st.session_state["sampleWord"]["Czech"].values[0])
 
     if st.button("Ukaž příklad"):
-        st.session_state["sampleWord"] = sample()
-        st.write(st.session_state["sampleWord"]["Czech"])
-        st.write_stream(getExample(st.session_state["sampleWord"]["Czech"]))
+        example = getExample(st.session_state["sampleWord"]["Czech"].values[0])
+        st.session_state["example"] = example
+
+        st.session_state["state"] = "example"
+    
+    if st.session_state["state"] == "example":
+        st.write(st.session_state["example"])
+        
         
     if st.button("Ukaž odpověd"):
-
-        st.write(st.session_state["sampleWord"]["Czech"])
-        st.write(st.session_state["sampleWord"]["English"])
+        st.write(st.session_state["sampleWord"]["English"].values[0])
+        if st.session_state["state"] == "example":
+            st.write(getTranslation(st.session_state["example"]))
+        
+        st.session_state["state"] = "answer"
 
     
 with tab2:
